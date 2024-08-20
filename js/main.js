ed4388e68a1dd600f9eaa85ebace6ca7256893a8
yjsun <yjsun0123@daum.net>
2024-08-20 13:27:22 +0900

    add responsive style

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
  /* ------------------------- header --------------------------- */
- let header = document.querySelector(".header");
- let headerHeight = header.offsetHeight;
+ const header = document.querySelector(".header");
+ const headerHeight = header.offsetHeight;
  
  window.onscroll = function () {
-   let windowTop = window.scrollY;
+   const windowTop = window.scrollY;
    if (windowTop >= headerHeight) {
      header.classList.add("header--active");
    } else {
      header.classList.remove("header--active");
    }
  };
+
+const headerMenus = document.querySelectorAll('.header__menu li');
+for (const headerMenu of headerMenus) {
+    headerMenu.addEventListener('click', function () {
+    const scrollPosition = document.querySelector(this.dataset.target).offsetTop;
+    window.scrollTo({top:scrollPosition, behavior:'smooth'});
+  });
+}
